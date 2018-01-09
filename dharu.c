d901ba0eed8d1f084f85d927bc5ad9eff2717989
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-09 16:14:10 +0530

    Create dharu.c

---
+++ dharu.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a=1,b=2,c=3;
+ if(a>b&&a>c)
+ {
+  printf("a is largest number");
+  }
+  else if(a<b&&a<c)
+  {
+   printf("b is largest number");
+   }
+   else
+   {
+    printf("c is largest number");
+    }
+    }

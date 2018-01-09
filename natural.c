21600a33b53070bbbe25a84d1867c2e6487f03f7
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2018-01-09 16:36:26 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,i,n;
+ printf("Enter the positive number");
+ scanf("%d",&a);
+ for(i=0;i<n;i++)
+ {
+  sum=sum+n;
+  printf("The number is natural");
+  }
+  }
